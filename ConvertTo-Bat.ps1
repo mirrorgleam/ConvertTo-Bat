998d3d65db49346450a65b2cd20bdd87d839e7b3
mirrorgleam <mirrorgleam@yahoo.com>
2016-08-29 09:00:35 -0400

    Initial ps1 file upload

---
+++ ConvertTo-Bat.ps1
@@ -0,0 +1,26 @@
+﻿Function ConvertTo-Bat
+{
+<#
+.SYNOPSIS
+Takes input of a .ps1 file and converts it to a base64 encoded .bat powershell command.
+.DESCRIPTION
+
+#>
+
+param
+    (
+    [parameter(Mandatory=$true)]
+    [string]
+    $pathToPs1,
+    [parameter(Mandatory=$true)]
+    [string]
+    $pathToOutput 
+    )
+
+$code = Get-Command $pathToPs1 |select -ExpandProperty ScriptBlock
+
+$Base64 = [convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($code))
+
+"@PowerShell -EncodedCommand $Base64" |Out-File -Encoding ascii -FilePath $pathToOutput
+
+}
